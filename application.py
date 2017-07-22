81eb58f45dd9e47bf5c47daec28c5fbcad37e100
Joey C <joeycyq@gmail.com>
2017-07-22 16:11:01 +0800

    Remove Enum lookup tables

--- application.py
+++ application.py
@@ -40,16 +40,32 @@ class Result(object):
         return json.dumps(self.__dict__, ensure_ascii=False)
 
 
-class UpdateType(Enum):
-    MESSAGE = 0
-    EDITED_MESSAGE = 1
-    CHANNEL_POST = 2
-    EDITED_CHANNEL_POST = 3
-    INLINE_QUERY = 4
-    CHOSEN_INLINE_RESULT = 5
-    CALLBACK_QUERY = 6
-    SHIPPING_QUERY = 7
-    PRE_CHECKOUT_QUERY = 8
+class Update(object):
+    class Type(Enum):
+        MESSAGE = "message"
+        EDITED_MESSAGE = "edited_message"
+        CHANNEL_POST = "channel_post"
+        EDITED_CHANNEL_POST = "edited_channel_post"
+        INLINE_QUERY = "inline_query"
+        CHOSEN_INLINE_RESULT = "chosen_inline_result"
+        CALLBACK_QUERY = "callback_query"
+        SHIPPING_QUERY = "shipping_query"
+        PRE_CHECKOUT_QUERY = "pre_checkout_query"
+
+    class Field(Enum):
+        UPDATE_ID = "update_id"
+        FROM = "from"
+        ID = "id"
+        CHAT = "chat"
+        MESSAGE = "message"
+        SUCCESSFUL = "ok"
+        DESCRIPTION = "description"
+        RESULT = "result"
+        INLINE_QUERY = "inline_query"
+        QUERY = "query"
+        TEXT = "text"
+        DATE = "date"
+        MESSAGE_ID = "message_id"
 
 
 # Track the latest unique updates to prevent spamming users with multiple responses.
@@ -81,54 +97,12 @@ processed_updates = SortedDictWithMaxSize("tracker.processed_updates")
 ignored_updates = SortedDictWithMaxSize("tracker.ignored_updates")
 
 
-update_types = {"message": UpdateType.MESSAGE,
-                "edited_message": UpdateType.EDITED_MESSAGE,
-                "channel_post": UpdateType.CHANNEL_POST,
-                "edited_channel_post": UpdateType.EDITED_CHANNEL_POST,
-                "inline_query": UpdateType.INLINE_QUERY,
-                "chosen_inline_result": UpdateType.CHOSEN_INLINE_RESULT,
-                "callback_query": UpdateType.CALLBACK_QUERY,
-                "shipping_query": UpdateType.SHIPPING_QUERY,
-                "pre_checkout_query": UpdateType.PRE_CHECKOUT_QUERY}
-
-
-class Field(Enum):
-    UPDATE_ID = 0
-    FROM = 1
-    ID = 2
-    CHAT = 3
-    MESSAGE = 4
-    SUCCESSFUL = 5
-    DESCRIPTION = 6
-    RESULT = 7
-    INLINE_QUERY = 8
-    QUERY = 9
-    TEXT = 10
-    DATE = 11
-    MESSAGE_ID = 12
-
-
-fields = {Field.UPDATE_ID: "update_id",
-          Field.FROM: "from",
-          Field.ID: "id",
-          Field.CHAT: "chat",
-          Field.MESSAGE: "message",
-          Field.SUCCESSFUL: "ok",
-          Field.DESCRIPTION: "description",
-          Field.RESULT: "result",
-          Field.INLINE_QUERY: "inline_query",
-          Field.QUERY: "query",
-          Field.TEXT: "text",
-          Field.DATE: "date",
-          Field.MESSAGE_ID: "message_id"}
-
-
 def check_response(response):
     response_json = response.json()
-    successful = response_json[fields[Field.SUCCESSFUL]]
+    successful = response_json[Update.Field.SUCCESSFUL.value]
     response_text = ""
     if not successful:
-        response_text = response_json[fields[Field.DESCRIPTION]]
+        response_text = response_json[Update.Field.DESCRIPTION.value]
     return successful, response_text
 
 
@@ -139,14 +113,14 @@ def send_message(chat_id, message_text):
 
 
 def message_to_bot(update, update_id):
-    user_id = update[fields[Field.MESSAGE]][fields[Field.FROM]][fields[Field.ID]]
+    user_id = update[Update.Field.MESSAGE.value][Update.Field.FROM.value][Update.Field.ID.value]
     logging.getLogger("user").info("id: " + str(user_id))
 
-    message = update[fields[Field.MESSAGE]]
-    message_id = message[fields[Field.MESSAGE_ID]]
-    message_date = message[fields[Field.DATE]]
-    message_text = message[fields[Field.TEXT]]
-    chat_id = message[fields[Field.CHAT]][fields[Field.ID]]
+    message = update[Update.Field.MESSAGE.value]
+    message_id = message[Update.Field.MESSAGE_ID.value]
+    message_date = message[Update.Field.DATE.value]
+    message_text = message[Update.Field.TEXT.value]
+    chat_id = message[Update.Field.CHAT.value][Update.Field.ID.value]
     logging.getLogger("user.message").debug(
         "Message " + str(message_id) +
         " at " + str(message_date) +
@@ -193,9 +167,9 @@ def new_user(update_id, chat_id, user_id, message_id):
 
 
 def tinify(update, update_id):
-    inline_query = update[fields[Field.INLINE_QUERY]]
-    query_id = inline_query[fields[Field.ID]]
-    query = inline_query[fields[Field.QUERY]]
+    inline_query = update[Update.Field.INLINE_QUERY.value]
+    query_id = inline_query[Update.Field.ID.value]
+    query = inline_query[Update.Field.QUERY.value]
     result = Result(query)
     answer = {"inline_query_id": query_id, "results": [result.__dict__]}
     response = outgoing_requests.post(api_answer_inline_query, json=answer)
@@ -214,8 +188,8 @@ def tinify(update, update_id):
     return ""
 
 
-routers = {UpdateType.MESSAGE: message_to_bot,
-           UpdateType.INLINE_QUERY: tinify}
+routers = {Update.Type.MESSAGE: message_to_bot,
+           Update.Type.INLINE_QUERY: tinify}
 
 
 @application.route("/" + TELEGRAM_TOKEN, methods=['POST'])
@@ -223,18 +197,16 @@ def route_message():
     result = ""
 
     update = request.get_json()
-    update_id = update[fields[Field.UPDATE_ID]]
+    update_id = update[Update.Field.UPDATE_ID.value]
     if update_id in processed_updates.values() or update_id in ignored_updates.values():
         logging.getLogger("tracker").info("Ignoring update " + str(update_id) + ".")
         return result
 
-    update_type = list(filter(
-        lambda possible_update_type: possible_update_type in update,
-        update_types.keys()))[0]
+    update_type = list(filter(lambda possible_type: possible_type.value in update,
+                              Update.Type))[0]
 
-    if update_types[update_type] in routers:
-        route = routers[update_types[update_type]]
-        result = route(update, update_id)
+    if update_type in routers:
+        result = routers[update_type](update, update_id)
     else:
         ignored_updates.add(update_id)
 
